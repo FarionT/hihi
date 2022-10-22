a9a077201d97ef984f9fd51777c2994df1ce875c
FarionT <farion2803@gmail.com>
2022-10-22 15:00:51 +0700

    hehe

---
+++ compe.php
@@ -0,0 +1,148 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+    <nav>
+
+    </nav>
+    <div class="container col col-lg-10">
+        <div class="container text-center my-5">
+            <div id="pageTitle" class="mb-3 h1" >COMPTETITIONS</div>
+            <p style="font-size:small" class="mb-0">Copywriting Copywriting Copywriting</p>
+            <p style="font-size:small" class="mb-0">Copywriting Copywriting Copywriting</p>
+        </div>
+        <div id="competitions" class="my-5">
+            <div id="firstCompetition" class="d-flex justify-content-lg-start justify-content-center mx-5 my-5">
+                <div class="d-flex row row-md-2 justify-content-center" id="upCompetition">
+                    <div id="upLeft" class="d-flex mx-4 my-lg-0 my-4 col justify-content-center">
+                        <div class="bg-secondary rounded-circle col-auto" style="width:170px; height:170px">
+    
+                        </div>
+                    </div>
+                    <div id="upRight" class="mx-4 col col-auto">
+                        <div class="d-flex justify-content-lg-start justify-content-sm-center justify-content-center">
+                            <p class="mx-2 h3">Competition Title</p>
+                        </div>
+                        <div>
+                            <table class="table table-borderless d-flex justify-content-lg-start justify-content-center">
+                                <tr>
+                                    <td class="py-0">Theme</td>
+                                    <td class="py-0">theme theme</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-0">Start</td>
+                                    <td class="py-0">DD MM YYYY</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-0">End</td>
+                                    <td class="py-0">DD MM YYYY</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-0"></td>
+                                    <td class="py-0">pukul 00:00 WIB</td>
+                                </tr>
+                            </table>
+                            <div class="d-flex justify-content-center justify-content-lg-start">
+                                <a href="#" class="btn btn-light border-dark rounded">Register</a>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="secondCompetition" class="d-flex justify-content-center justify-content-lg-end mx-5 my-5">
+                <div class="d-flex row row-md-2 justify-content-center" id="upCompetition">
+                    <div id="upLeft" class="d-flex mx-4 my-lg-0 my-4 col justify-content-center">
+                        <div class="bg-secondary rounded-circle col-auto" style="width:170px; height:170px">
+    
+                        </div>
+                    </div>
+                    <div id="upRight" class="mx-4 col col-auto">
+                        <div class="d-flex justify-content-lg-start justify-content-center">
+                            <p class="mx-2 h3">Competition Title</p>
+                        </div>
+                        <div>
+                            <table class="table table-borderless d-flex justify-content-lg-start justify-content-center">
+                                <tr>
+                                    <td class="py-0">Theme</td>
+                                    <td class="py-0">theme theme</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-0">Start</td>
+                                    <td class="py-0">DD MM YYYY</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-0">End</td>
+                                    <td class="py-0">DD MM YYYY</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-0"></td>
+                                    <td class="py-0">pukul 00:00 WIB</td>
+                                </tr>
+                            </table>
+                            <div class="d-flex justify-content-center justify-content-lg-start">
+                                <a href="#" class="btn btn-light border-dark rounded">Register</a>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="thirdCompetition" class="d-flex justify-content-lg-start justify-content-center mx-5 my-5">
+                <div class="d-flex row row-md-2 justify-content-center" id="upCompetition">
+                    <div id="upLeft" class="d-flex mx-4 my-lg-0 my-4 col justify-content-center">
+                        <div class="bg-secondary rounded-circle col-auto" style="width:170px; height:170px">
+    
+                        </div>
+                    </div>
+                    <div id="upRight" class="mx-4 col col-auto">
+                        <div class="d-flex justify-content-lg-start justify-content-sm-center justify-content-center">
+                            <p class="mx-2 h3">Competition Title</p>
+                        </div>
+                        <div>
+                            <table class="table table-borderless d-flex justify-content-lg-start justify-content-center">
+                                <tr>
+                                    <td class="py-0">Theme</td>
+                                    <td class="py-0">theme theme</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-0">Start</td>
+                                    <td class="py-0">DD MM YYYY</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-0">End</td>
+                                    <td class="py-0">DD MM YYYY</td>
+                                </tr>
+                                <tr>
+                                    <td class="py-0"></td>
+                                    <td class="py-0">pukul 00:00 WIB</td>
+                                </tr>
+                            </table>
+                            <div class="d-flex justify-content-center justify-content-lg-start">
+                                <a href="#" class="btn btn-light border-dark rounded">Register</a>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="my-5 d-block text-center">
+        <p class="d-block mb-2">CONTACT US</p>
+        <div class="d-flex justify-content-center">
+            <div class="bg-secondary rounded mx-2" style="width:40px;height:40px"></div>
+            <a href="#" class="text-decoration-none align-self-center mx-2">GDC UMN</a>
+        </div>
+    </footer>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
